b016c7bd9f6ce1dfb7558f4e8237db73eb1f446e
杨杰 <jieyangb@ctrip.com>
2019-04-01 22:09:46 +0800

    -功能[  uikit ] -备注[ ]

---
+++ Swift_SweetSugar/Swift_SweetSugar/Extension/UIKit+Extension.swift
@@ -0,0 +1,96 @@
+//
+//  UIKit+Extension.swift
+//  Swift_SweetSugar
+//
+//  Created by mumu on 2019/4/1.
+//  Copyright © 2019年 Mumu. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    /// x
+    var mm_x: CGFloat {
+        get {
+            return frame.origin.x
+        }
+        set(newValue) {
+            var tempFrame: CGRect = frame
+            tempFrame.origin.x    = newValue
+            frame                 = tempFrame
+        }
+    }
+    
+    /// y
+    var mm_y: CGFloat {
+        get {
+            return frame.origin.y
+        }
+        set(newValue) {
+            var tempFrame: CGRect = frame
+            tempFrame.origin.y    = newValue
+            frame                 = tempFrame
+        }
+    }
+    
+    /// height
+    var mm_height: CGFloat {
+        get {
+            return frame.size.height
+        }
+        set(newValue) {
+            var tempFrame: CGRect = frame
+            tempFrame.size.height = newValue
+            frame                 = tempFrame
+        }
+    }
+    
+    /// width
+    var mm_width: CGFloat {
+        get {
+            return frame.size.width
+        }
+        set(newValue) {
+            var tempFrame: CGRect = frame
+            tempFrame.size.width = newValue
+            frame = tempFrame
+        }
+    }
+    
+    /// size
+    var mm_size: CGSize {
+        get {
+            return frame.size
+        }
+        set(newValue) {
+            var tempFrame: CGRect = frame
+            tempFrame.size = newValue
+            frame = tempFrame
+        }
+    }
+    
+    /// centerX
+    var mm_centerX: CGFloat {
+        get {
+            return center.x
+        }
+        set(newValue) {
+            var tempCenter: CGPoint = center
+            tempCenter.x = newValue
+            center = tempCenter
+        }
+    }
+    
+    /// centerY
+    var mm_centerY: CGFloat {
+        get {
+            return center.y
+        }
+        set(newValue) {
+            var tempCenter: CGPoint = center
+            tempCenter.y = newValue
+            center = tempCenter;
+        }
+    }
+}
---
+++ Swift_SweetSugar/Swift_SweetSugar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift_SweetSugar
+//
+//  Created by mumu on 2019/4/1.
+//  Copyright © 2019年 Mumu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
