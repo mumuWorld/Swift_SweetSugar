12010e3f504bc4ba1e13b101206fcfc6722fee62
杨杰 <yangjie01@rd.netease.com>
2021-09-29 11:13:37 +0800

    mod: coreText

---
+++ Swift_SweetSugar/Swift_SweetSugar/TestVC/CoreText/MMCoreTextVC.swift
@@ -0,0 +1,30 @@
+//
+//  MMCoreTextVC.swift
+//  Swift_SweetSugar
+//
+//  Created by 杨杰 on 2021/9/27.
+//  Copyright © 2021 Mumu. All rights reserved.
+//
+
+import UIKit
+
+class MMCoreTextVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
