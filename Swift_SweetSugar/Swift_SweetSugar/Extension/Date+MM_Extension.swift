0612cc1d437c0e368bebed5e347301d54ab6a18a
yangjie01 <617958070@qq.com>
2022-01-22 10:34:32 +0800

    add: pagecontrol

--- Swift_SweetSugar/Swift_SweetSugar/Extension/Date+MM_Extension.swift
+++ Swift_SweetSugar/Swift_SweetSugar/Extension/Date+MM_Extension.swift
@@ -12,6 +12,7 @@ enum kDateFormatterKey: String {
     case Default = "yyyy-MM-dd HH:mm:ss"
     case ShortYMD = "yyyy-MM-dd"
     case ShortYMDHM = "yyyy-MM-dd HH:mm"
+    case ShortMD = "MM-dd"
 }
 
 extension Date {
@@ -22,6 +23,14 @@ extension Date {
         return Int(time)
     }
     
+    static func currentDateStr(formatter: kDateFormatterKey = kDateFormatterKey.Default) -> String {
+            let date = Date()
+            shareFormatter.locale = Locale.current
+            shareFormatter.timeZone = TimeZone.current
+            shareFormatter.dateFormat = formatter.rawValue
+            let result = shareFormatter.string(from: date)
+            return result
+    }
     /// 根据时间戳 int 返回 格式日期字符串
     ///
     /// - Parameters:
@@ -35,5 +44,21 @@ extension Date {
         shareFormatter.dateFormat = formatter.rawValue
         let result = shareFormatter.string(from: date)
         return result
+    }
+    
+    static func getDayMonthYear(dateStr: String, formate: YDTDateFormatterStr = .yyyyMMddHHmm) -> (day: Int, month: Int, year: Int) {
+        formatter.dateFormat = formate.rawValue
+        guard let date = formatter.date(from: dateStr) else {
+            return (0,0,0)
+        }
+        return getDMY(with: date)
+    }
+    
+    static func getDMY(with date: Date) -> (day: Int, month: Int, year: Int) {
+        let dataComponents = canlendar.dateComponents([.day , .month , .year], from: date)
+        let day = dataComponents.day
+        let month = dataComponents.month
+        let year = dataComponents.year
+        return (day ?? 0, month ?? 0, year ?? 0)
     }
 }
---
+++ Swift_SweetSugar/Swift_SweetSugar/TestVC/UI测试/View/MMPageControl.swift
@@ -0,0 +1,92 @@
+//
+//  MMPageControl.swift
+//  TianQi
+//
+//  Created by Mumu on 2022/1/15.
+//
+
+import UIKit
+
+class MMPageControl: UIView {
+
+    lazy var configure: MMPageControlConfigure = MMPageControlConfigure()
+    
+    lazy var cacheSubviews: [UIView] = []
+    
+    var fitWidth: CGFloat = 0
+    
+    var curIndex: Int = 0
+    
+    
+    func updateConfigure(pConfigure: MMPageControlConfigure, curIndex: Int = 0) -> Void {
+        configure = pConfigure
+        removeSubviews()
+        for _ in 0..<configure.count {
+            createPageItem()
+        }
+        var curX: CGFloat = 0
+        subviews.enumerated().forEach { (index, item) in
+            if index == curIndex {
+                item.backgroundColor = configure.selectedColor
+                item.size = CGSize(width: configure.itemSelectedW, height: configure.itemH)
+            }
+            item.x = curX
+            curX = item.frame.maxX + configure.itemSpacing
+        }
+        self.curIndex = curIndex
+        //宽度就是减去最后一个空隙
+        fitWidth = curX - configure.itemSpacing
+    }
+    
+    func createPageItem() -> Void {
+        let item = UIView()
+        item.backgroundColor = configure.color
+        item.size = CGSize(width: configure.itemW, height: configure.itemH)
+        item.layer.cornerRadius = configure.itemCornerRaidus
+        addSubview(item)
+    }
+}
+
+extension MMPageControl {
+    
+    func setIndex(_ index: Int) -> Void {
+        if curIndex == index, subviews.containsIndex(index) {
+            return
+        }
+        curIndex = index
+        UIView.animate(withDuration: 0.2) {
+            var curX: CGFloat = 0
+            for (tmpIndex, item) in self.subviews.enumerated() {
+                if tmpIndex == index {
+                    item.width = self.configure.itemSelectedW
+                    item.backgroundColor = self.configure.selectedColor
+                } else {
+                    item.width = self.configure.itemW
+                    item.backgroundColor = self.configure.color
+                }
+                item.x = curX
+                curX = item.frame.maxX + self.configure.itemSpacing
+            }
+        }
+    }
+}
+
+struct MMPageControlConfigure {
+    
+    var count: Int = 0
+    
+    var color: UIColor = UIColor.hexStringToColor("D8D8D8")
+    
+    var selectedColor: UIColor = UIColor.hexStringToColor("208CF7")
+    
+    var itemH: CGFloat = 4
+    
+    var itemW: CGFloat = 4
+    
+    var itemSelectedW: CGFloat = 12
+    
+    var itemSpacing: CGFloat = 4
+    
+    var itemCornerRaidus: CGFloat = 2
+    
+}
